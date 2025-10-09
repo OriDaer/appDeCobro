0974593bf3f65c592edbc24f5f7571c5c16c2cba
OriDaer <d47709740@alumnos.itr.edu.ar>
2025-10-09 10:17:57 -0300

    .

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
+
 import os
 import mercadopago
 from dotenv import load_dotenv
